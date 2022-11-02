29a40739684afa3c5678c6403d034357b21e7eff
MossTeMuerA <emresevuk@gmail.com>
2022-11-02 12:26:15 +0300

    guncelleme

---
+++ Assets/A1_SuperMarketIdle/Scripts/ObserverPattern/LevelManager/LevelPowerUpOfficer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPowerUpOfficer : MonoBehaviour
+{
+    [SerializeField] bool speedUpActive = false, coinBoostActive = false;
+
+    public void FillTheRoomsItemStands() 
+    {
+    
+    }
+    public void SpeedUpTheCustomersForSomeTime(float speedBoostCoefficient, float duration) 
+    {
+    
+    }
+
+    public void GetCoinReward()
+    {
+
+    }
+
+    public void CoinEarnBoostForSomeTime(float coinBoostCoefficient, float duration) 
+    {
+
+    }
+
+    
+}
